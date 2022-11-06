931b40183dcdf2b2795e7860cda6cc7173347473
Tsaqif <mushoddaqt@gmail.com>
2022-11-07 06:11:07 +0700

    desain full

--- dist/js/script.js
+++ dist/js/script.js
@@ -4,4 +4,3 @@ var item = document.getElementById("main");
 //   if (e.deltaY > 0) item.scrollLeft += 100;
 //   else item.scrollLeft -= 100;
 // });
-console.log(item);
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,19 @@ module.exports = {
   content: ["*.{html,js}"],
   theme: {
     extend: {
+      screens: {
+        xs: "320px",
+      },
       fontFamily: {
         playfair: ["Playfair Display"],
         dancing: ["Dancing Script"],
+        poppins: ["Poppins"],
       },
       colors: {
         biruTua: "#1E3135",
+      },
+      fontSize: {
+        xxs: "0.65rem",
       },
     },
   },
